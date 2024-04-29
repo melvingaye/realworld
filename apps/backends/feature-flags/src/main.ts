a4c7536e569002ec2cce8d6a7f9bc806a4e84253
melvingaye <melvind.gaye@gmail.com>
2024-04-29 14:49:21 -0400

    chore: added swagger to services

--- apps/backends/feature-flags/src/main.ts
+++ apps/backends/feature-flags/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('RealWorld - Feature Flags')
+    .setDescription('Feature Flags for the RealWorld')
+    .setVersion('1.0')
+    .addTag('feature-flags')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
