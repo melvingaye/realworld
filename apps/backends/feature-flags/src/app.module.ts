2a2cd9a0f61c7be43383c4f32f7114d7dd329ef5
melvingaye <melvind.gaye@gmail.com>
2024-04-29 15:44:00 -0400

    chore: added web components

--- apps/backends/feature-flags/src/app.module.ts
+++ apps/backends/feature-flags/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OpenFeatureModule, InMemoryProvider } from '@openfeature/nestjs-sdk';
 
 @Module({
-  imports: [],
+  imports: [
+    OpenFeatureModule.forRoot({
+      defaultProvider: new InMemoryProvider({
+        testBooleanFlag: {
+          defaultVariant: 'default',
+          variants: { default: true },
+          disabled: false,
+        },
+      }),
+      providers: {
+        differentProvider: new InMemoryProvider(),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
